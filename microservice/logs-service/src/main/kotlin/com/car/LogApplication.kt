d01d46ce79393d63dcf5e04fde724f26b2e59d79
Savaş Dede <svsdd22@gmail.com>
2023-12-12 11:26:20 +0300

    develop fix

--- microservice/logs-service/src/main/kotlin/com/car/LogApplication.kt
+++ microservice/logs-service/src/main/kotlin/com/car/LogApplication.kt
@@ -2,8 +2,10 @@ package com.car
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class LogApplication
 
 fun main(args: Array<String>) {
