1f1fcca3be643018e49989155329314569255cd4
Savaş Dede <svsdd22@gmail.com>
2023-12-12 10:50:49 +0300

    develop fix

--- microservice/discovery-server/src/main/kotlin/com/car/DiscoveryApplication.kt
+++ microservice/discovery-server/src/main/kotlin/com/car/DiscoveryApplication.kt
@@ -2,8 +2,10 @@ package com.car
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class DiscoveryApplication
 
 fun main(args: Array<String>) {
