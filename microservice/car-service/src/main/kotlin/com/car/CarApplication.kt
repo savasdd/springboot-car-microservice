4603fcea057b1f7ca616afd1da9b49364bf0f0f6
Savaş Dede <svsdd22@gmail.com>
2023-12-12 11:10:15 +0300

    develop fix

--- microservice/car-service/src/main/kotlin/com/car/CarApplication.kt
+++ microservice/car-service/src/main/kotlin/com/car/CarApplication.kt
@@ -2,10 +2,12 @@ package com.car
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class CarApplication
 
 fun main(args: Array<String>) {
-    runApplication<CarApplication>(*args)
+  runApplication<CarApplication>(*args)
 }
